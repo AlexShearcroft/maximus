0b0e363b9b09db8db81cf3f334aeb4283acfef06
alexshearcroft <alex@stormcreative.co.uk>
2017-02-04 21:02:15 +0000

    testing new framework

--- resources/views/admin/articles/view.blade.php
+++ resources/views/admin/articles/view.blade.php
@@ -5,7 +5,7 @@
         <h1>{{ $article->title }}</h1>
 
         <article>
-            {{ $body }}
+            {{ $article->body }}
         </article>
 
         @unless ($article->tags->isEmpty())
